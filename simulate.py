89de92f95b52eefd5aefbbbaa8f619929efe5d61
Brian Bowman <brianbowman@ip0af503cb.int.uvm.edu>
2024-02-15 14:05:15 -0500

    robot slides small distances back and forth

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import numpy as np
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import random
 import time
 
 
@@ -24,7 +25,8 @@ for i in range(0, 100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = 1, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = 1, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-3.14/2.0, 3.14/2.0), maxForce = 50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = 1, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-3.14/2.0, 3.14/2.0), maxForce = 50)
     time.sleep(SLEEP_TIME)
 
 p.disconnect()
