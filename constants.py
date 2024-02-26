3cf2ae5c9c2f2391de4244a2caea4fba241bb969
Brian Bowman <brianbowman@ip0af5c2b2.int.uvm.edu>
2024-02-26 13:36:59 -0500

    started refactor

---
+++ constants.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+SLEEP_TIME = 1 / 240
+
+BackLeg_amplitude = np.pi / 4
+BackLeg_frequency = 15
+BackLeg_phaseOffset = 0
+FrontLeg_amplitude = np.pi / 4
+FrontLeg_frequency = 2.5
+FrontLeg_phaseOffset = 0
--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import constants as c
 import numpy as np
 import pybullet as p
 import pybullet_data
@@ -5,15 +6,6 @@ import pyrosim.pyrosim as pyrosim
 import random
 import time
 
-
-SLEEP_TIME = 1 / 240
-
-BackLeg_amplitude = np.pi / 4
-BackLeg_frequency = 7.5
-BackLeg_phaseOffset = 0
-FrontLeg_amplitude = np.pi / 4
-FrontLeg_frequency = 2.5
-FrontLeg_phaseOffset = 0
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -29,9 +21,9 @@ backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
 BackLeg_targetAngles = np.linspace(0, np.pi*2, 1000)
-BackLeg_targetAngles = BackLeg_amplitude * np.sin(BackLeg_frequency * BackLeg_targetAngles + BackLeg_phaseOffset)
+BackLeg_targetAngles = c.BackLeg_amplitude * np.sin(c.BackLeg_frequency * BackLeg_targetAngles + c.BackLeg_phaseOffset)
 FrontLeg_targetAngles = np.linspace(0, np.pi*2, 1000)
-FrontLeg_targetAngles = FrontLeg_amplitude * np.sin(FrontLeg_frequency * FrontLeg_targetAngles + FrontLeg_phaseOffset)
+FrontLeg_targetAngles = c.FrontLeg_amplitude * np.sin(c.FrontLeg_frequency * FrontLeg_targetAngles + c.FrontLeg_phaseOffset)
 
 #np.save("/Users/brianbowman/Desktop/robotics/data/backLegtargetAngles.npy", BackLeg_targetAngles)
 #np.save("/Users/brianbowman/Desktop/robotics/data/frontLegtargetAngles.npy", FrontLeg_targetAngles)
@@ -45,7 +37,7 @@ for i in range(0, 1000):
     pyrosim.Set_Motor_For_Joint(bodyIndex = 1, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = BackLeg_targetAngles[i], maxForce = 50)
     pyrosim.Set_Motor_For_Joint(bodyIndex = 1, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = FrontLeg_targetAngles[i], maxForce = 50)
 
-    time.sleep(SLEEP_TIME)
+    time.sleep(c.SLEEP_TIME)
 
 p.disconnect()
 np.save("/Users/brianbowman/Desktop/robotics/data/sensor_values.npy", backLegSensorValues)
