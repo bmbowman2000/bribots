5ef6d6dc6689737a7ac31651e08a2df94651a588
Brian Bowman <brianbowman@Brians-MacBook-Air-2.local>
2024-03-19 17:06:22 -0400

    refactored generate and simulate into solution

--- search.py
+++ search.py
@@ -1,5 +1,11 @@
+from hillclimber import HILL_CLIMBER
 import os
 
+"""
 for i in range(5):
     os.system("python3 generate.py")
     os.system("python3 simulate.py")
+"""
+
+hc = HILL_CLIMBER()
+hc.Evolve()
